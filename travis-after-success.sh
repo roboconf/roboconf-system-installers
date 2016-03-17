dc0c360f07e212aecf5796a24ac26fb0ac020713
Vincent Zurczak <vincent.zurczak@gmail.com>
2016-03-17 09:22:56 +0100

    #5 Add continuous integration on the Debian packages

--- travis-after-success.sh
+++ travis-after-success.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# 1. Copy Maven settings on the VM.
-# 2. Upload artifacts to Sonatype.
-# 3. Use -q option to only display Maven errors and warnings.
-# 4. Use --settings to force the usage of our "settings.xml" file.
+# 1. Upload artifacts to Sonatype.
+# 2. Use -q option to only display Maven errors and warnings.
+# 3. Use --settings to force the usage of our "settings.xml" file.
 
-wget http://roboconf.net/resources/build/settings.xml
 mvn clean deploy -DskipTests=true -q --settings settings.xml
